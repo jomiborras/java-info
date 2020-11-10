b183e1af86a9ee4de8986c72e5decff509f9a197
jomiborras <45013912+jomiborras@users.noreply.github.com>
2020-11-10 11:23:26 -0300

    Ejercicio: ingresar número para devolver días de la semana, usando switch

---
+++ EjercicioTres.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class EjercicioTres {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int dia = scan.nextInt();
+        scan.close();
+
+        switch(dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Lunes");
+                break;
+            case 3:
+                System.out.println("Martes");
+                break;
+            case 4:
+                System.out.println("Miércoles");
+                break;
+            case 5:
+                System.out.println("Jueves");
+                break;
+            case 6:
+                System.out.println("Viernes");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+        }
+    }
+}
